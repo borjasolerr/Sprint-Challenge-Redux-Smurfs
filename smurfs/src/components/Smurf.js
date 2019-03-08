6e6b9f605683c7be59c16c9219f53085346bd54c
borjasolerr <borjasolrey@gmail.com>
2019-03-08 11:34:03 +0100

    Displayed smurfs data

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Smurf = props => {
+  return (
+    <div>
+      <h3>
+        {props.smurf.name} {props.smurf.age} {props.smurf.height}
+      </h3>
+    </div>
+  );
+};
+
+export default connect(
+  null,
+  {}
+)(Smurf);
